120954be7f6023f73abd0d391dbba8590a9072c2
Francisco Campos <sasasasamaes13@gmail.com>
2023-02-13 20:42:47 -0600

    UI de Login con Boostrap

--- 03-react-auth/src/pages/Login.jsx
+++ 03-react-auth/src/pages/Login.jsx
@@ -1,6 +1,39 @@
+import logo from '@/assets/react.svg'
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className='form-signin w-100 m-auto'>
+      <form>
+        <img className='mb-4' src={logo} alt='' width='72' height='57' />
+        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='floatingInput'
+            name='email'
+            value=''
+            onChange={() => {}}
+            placeholder='name@example.com'
+          />
+          <label htmlFor='floatingInput'>Email address</label>
+        </div>
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='floatingPassword'
+            name='password'
+            value=''
+            onChange={() => {}}
+            placeholder='Password'
+          />
+          <label htmlFor='floatingPassword'>Password</label>
+        </div>
+        <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign in</button>
+        <p className='mt-5 mb-3 text-muted'>© 2017–2023</p>
+      </form>
+    </main>
   )
 }
 export default Login

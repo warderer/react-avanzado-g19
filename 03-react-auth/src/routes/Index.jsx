c022d005a078575b36103f390cbb7e88f9e3175b
Francisco Campos <sasasasamaes13@gmail.com>
2023-02-15 19:30:26 -0600

    Componente Dashboard Protegido por Ruta

--- 03-react-auth/src/routes/Index.jsx
+++ 03-react-auth/src/routes/Index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import {
   Home,
   Dashboard,
@@ -7,12 +7,15 @@ import {
   Secret,
   Signup
 } from '@/pages'
+import { AuthContext } from '@/context/AuthContext'
 
 const RouterIndex = () => {
+  const { isAuth } = useContext(AuthContext)
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/dashboard' element={isAuth ? <Dashboard /> : <Navigate to='/login' replace />} />
       <Route path='/login' element={<Login />} />
       <Route path='/secret' element={<Secret />} />
       <Route path='/Signup' element={<Signup />} />

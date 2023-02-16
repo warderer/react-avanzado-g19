abe69e77f2f20a30e667542e36e7357659b51de3
Francisco Campos <sasasasamaes13@gmail.com>
2023-02-15 19:55:07 -0600

    Refactor UserServices interceptors

--- 03-react-auth/src/services/userServices.js
+++ 03-react-auth/src/services/userServices.js
@@ -1,19 +1,23 @@
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:3000'
+// Referencia: https://stackoverflow.com/questions/71989146/axios-instance-not-getting-the-token-from-local-storage-on-first-call-react-js
 
-const token = window.localStorage.getItem('token')
-
-const config = {
-  headers: {
-    Authorization: `JWT ${token}`
+axios.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = token ? `JWT ${token}` : ''
   }
-}
+  return config
+},
+(error) => {
+  return Promise.reject(error)
+})
 
 const registerUser = (data) => axios.post(`${BASE_URL}/register`, data)
 
 const loginUser = (data) => axios.post(`${BASE_URL}/login`, data)
 
-const getSingleUser = (id) => axios.get(`${BASE_URL}/users/${id}`, config)
+const getSingleUser = (id) => axios.get(`${BASE_URL}/users/${id}`)
 
 export { registerUser, loginUser, getSingleUser }

ee8fcd9c21ded9f7d262dde0e516ed53ad01bc4f
Francisco Campos <sasasasamaes13@gmail.com>
2023-02-15 19:23:21 -0600

    Header con Context

--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import './Header.scss'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '@/context/AuthContext'
 
 const Header = () => {
+  const { isAuth, logout } = useContext(AuthContext)
   return (
     <nav className='header'>
       <Link to='/' className='header__logo'>LOGO</Link>
@@ -9,18 +12,33 @@ const Header = () => {
         <li className='header__list-item'>
           <Link to='/' className='header__item-link header__item-link--is-active'>Home</Link>
         </li>
-        <li className='header__list-item'>
-          <Link to='/dashboard' className='header__item-link'>Dashboard</Link>
-        </li>
-        <li className='header__list-item'>
-          <Link to='/secret' className='header__item-link'>Secret</Link>
-        </li>
-        <li className='header__list-item'>
-          <Link to='/login' className='header__item-link'>Login</Link>
-        </li>
-        <li className='header__list-item'>
-          <Link to='/signup' className='header__item-link'>Signup</Link>
-        </li>
+        {
+          !isAuth
+            ? (
+              <>
+                <li className='header__list-item'>
+                  <Link to='/login' className='header__item-link'>Login</Link>
+                </li>
+                <li className='header__list-item'>
+                  <Link to='/signup' className='header__item-link'>Signup</Link>
+                </li>
+              </>
+              )
+            : (
+              <>
+                <li className='header__list-item'>
+                  <Link to='/dashboard' className='header__item-link'>Dashboard</Link>
+                </li>
+                <li className='header__list-item'>
+                  <Link to='/secret' className='header__item-link'>Secret</Link>
+                </li>
+                <li className='header__list-item'>
+                  <Link to='/signup' className='header__item-link' onClick={logout}>Logout</Link>
+                </li>
+              </>
+              )
+        }
+
       </ul>
     </nav>
   )
